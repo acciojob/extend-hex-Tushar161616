af908a5ba0be0a406fa5f4d55c8f618c10fd5406
Tushar Sharma <145897973+Tushar161616@users.noreply.github.com>
2023-11-21 16:14:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let newstr = "#";
+	for(let i = 1; i<shortHex.length; i++)
+		{
+			let ch = shortHex.charAt(i);
+			for(let l = 0; l<2; l++)
+				{
+					newstr += ch;
+				}
+		}
+	// console.log(newstr);
+	return newstr;
 };
 
 // Do not change the code below.
